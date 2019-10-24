9c98c24ea5a87b5e9213e659e59d236854381683
Agostino De Marco <agostino.demarco@unina.it>
2019-10-24 10:33:45 +0200

    DotMap example from @3blue1brown

--- agodemar_example_scenes.py
+++ agodemar_example_scenes.py
@@ -125,6 +125,22 @@ class AddingMoreText(Scene):
         self.play(FadeOut(quote))
 
 
+class DotMap(Scene):
+    def construct(self):
+        dots = dict()
+        annos = dict()
+        var_index = 0
+        for x in range(-7, 8):
+            for y in range(-4, 5):
+                annos[f"{x}{y}"] = CustomTextMobject(f"$({x}, {y})$")
+                dots[f"{var_index}"] = Dot(np.array([x, y, 0]))
+                var_index = var_index + 1
+        for anno, dot in zip(annos.values(), dots.values()):
+            self.add(anno)
+            self.add(dot)
+            self.wait(0.2)
+            self.remove(anno)
+
 """
 Run this class with the command:
 
